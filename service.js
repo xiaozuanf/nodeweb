3531d2e84341e16aa18891d6970edbd6bf16429e
langdinggai <1845255483@qq.com>
2021-02-04 18:19:52 +0800

    sd

--- service.js
+++ service.js
@@ -20,7 +20,8 @@ var connection = mysql.createConnection({
   user     : 'root',              
   password : 'hgl131410040209',       
   port: '3306',                   
-  database: 'nodeweb' 
+  database: 'nodeweb' ,
+  multipleStatements: true//用于分页返回总条数
 }); 
  
 connection.connect();
@@ -68,10 +69,29 @@ app.get('/ariclelist', function (req, res) {
           })
   });
 })
+//文章列表分页
+app.get('/ariclepage', function (req, res) {
+  var  sql = 'SELECT COUNT(*) FROM article ; SELECT * FROM article ORDER BY date DESC LIMIT '+(req.query.page-1)*req.query.size+', '+req.query.size;
+  connection.query(sql,function (err, result) {
+    if(err){
+      console.log('[SELECT ERROR] - ',err.message);
+      res.json({
+        code:299,
+        data:err.message
+      })
+      return;
+    }
+    res.json({
+      code:200,
+      data:{
+        total:result[0][0]['COUNT(*)'],
+        list:result[1]
+      }
+    })
+  });
+})
 //文章列表详情
 app.get('/aricleinfo', function (req, res) {
-  console.log(req.body)
-  console.log(req.query)
   var  sql = 'SELECT * FROM article where id='+req.query.id;
   connection.query(sql,function (err, result) {
     if(err){
@@ -86,6 +106,24 @@ app.get('/aricleinfo', function (req, res) {
       code:200,
       data:result[0]
     })
+  });
+})
+//文章分类列表
+app.get('/aricletype', function (req, res) {
+  var  sql = 'SELECT * FROM articletype';
+  connection.query(sql,function (err, result) {
+          if(err){
+            console.log('[SELECT ERROR] - ',err.message);
+            res.json({
+              code:299,
+              data:err.message
+            })
+            return;
+          }
+          res.json({
+            code:200,
+            data:result
+          })
   });
 })
 //改
